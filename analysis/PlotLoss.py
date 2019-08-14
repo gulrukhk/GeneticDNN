da5d3eb14154f109003a3f12d0bd87d8c508f1c5
gulrukhk <gulrukhk@yahoo.com>
2019-08-14 01:36:40 -0700

    adding parser and comparison between training

--- analysis/PlotLoss.py
+++ analysis/PlotLoss.py
@@ -5,52 +5,124 @@ try:
     import cPickle as pickle
 except ImportError:
     import pickle
+import argparse
 
 def main():
-    lossfile = '../results/loss_history_simple.pkl'
-    losspath = 'results/loss_plots_simple_dropout/'
-    safe_mkdir(losspath)
-    with open(lossfile, 'rb') as f:
-         x = pickle.load(f)
-    train_loss = x['train'][5:]
-    test_loss = x['test'][5:]
-    color =2
-    PlotLossRoot(train_loss, 'Primary Energy Regression ( Train loss)', losspath + 'train_loss.pdf', color=color)
-    PlotAccuracyRoot(train_loss, 'Primary Energy Regression ( Train Accuracy)', losspath + 'train_accuracy.pdf', color=color)
-    PlotLossRoot(test_loss, 'Primary Energy Regression ( Test loss)', losspath + 'test_loss.pdf', color=color+2)
-    PlotAccuracyRoot(test_loss, 'Primary Energy Regression ( Test Accuracy)', losspath + 'test_accuracy.pdf', color=color+2)
+    parser = get_parser()
+    params = parser.parse_args()
+
+    losspath = params.losspath
+    lossfiles = params.lossfiles 
+    lossfiles.append('GA_mp_1_loss_history.pkl')
+    outpath = params.outpath
+    safe_mkdir(outpath)
+    start = params.start
+    stop = params.stop
+    loss_ids = []
+    for item in lossfiles:
+      loss_ids.append(item.split('_loss')[0])
+    losses =[]
+    for lfile in lossfiles:
+      with open(losspath + lfile, 'rb') as f:
+         losses.append(pickle.load(f))
+    for key in losses[0]:
+      if key!='timing' and len(losses[0][key]) > 1:
+        loss = []
+        time =[]
+        epochs = [] 
+        for item in losses:
+          loss.append(item[key][start:stop])
+          time_list =[]
+          for j, t in enumerate(item['timing'][start:stop]):
+            if j==0:
+              time_list.append(t)
+            else:
+              time_list.append(t + time_list[j-1])
+          time.append(time_list) 
+          epochs.append(np.arange(len(item[key])))
+        PlotLossRoot(loss,  loss_ids, epochs, 'Primary Energy Regression ({})'.format(key), 'epochs', outpath + key + '_loss.pdf')
+        PlotAccuracyRoot(loss, loss_ids, epochs, 'Primary Energy Regression({})'.format(key), 'epochs', outpath + key + '_accuracy.pdf')
+        PlotLossRoot(loss,  loss_ids, time, 'Primary Energy Regression ({})'.format(key), 'time [sec]', outpath + key + '_loss_time.pdf')
+        PlotAccuracyRoot(loss, loss_ids, time, 'Primary Energy Regression({})'.format(key), 'time [sec]', outpath + key + '_accuracy_time.pdf')
+
+def get_parser():
+    parser = argparse.ArgumentParser(description='Loss plots' )
+    parser.add_argument('--losspath', action='store', type=str, default='../results/history/', help='dir for loss history')
+    parser.add_argument('--outpath', action='store', type=str, default='results/loss_plots_plan5_mp1/', help='directory for results')
+    parser.add_argument('--lossfiles', nargs='+', default=['GA_plan5_time_loss_history.pkl'], help='loss history (list of pkl files delimited by spaces)')
+    parser.add_argument('--start', type=int, default=0, help='can be used to remove initial epochs')
+    parser.add_argument('--stop', type=int, default=1000, help='can be used to remove later epochs')
+    return parser
     
-def PlotLossRoot(loss_list, label, filename, color=2):
+def PlotLossRoot(loss_list, loss_ids, xaxis, label, xlabel, filename, color=2, style=1):
     c = ROOT.TCanvas('c' ,"" ,200 ,10 ,700 ,500)
     c.SetGrid()
-    loss_array = np.zeros(len(loss_list))
-    x_array = np.zeros(len(loss_list))
-    for i, l in enumerate(loss_list):
-      loss_array[i] = l
-      x_array[i] = i
-    graph = ROOT.TGraph(len(loss_list), x_array, loss_array)
-    graph.SetTitle(label + "Epochs; Loss;")
-    graph.SetLineColor(color)
-    graph.GetXaxis
-    graph.Draw()
+    legend = ROOT.TLegend(.7, .7, .9, .9)
+    mg = ROOT.TMultiGraph()
+    mg.SetTitle("{} ;{}; loss;".format(label, xlabel))
+    loss_list = equalize(loss_list)
+    xaxis = xaxis[:len(loss_list[0])]
+    graphs=[]
+    for index, item in enumerate(loss_list):
+      loss_array = np.zeros(len(item))
+      x_array = np.zeros(len(item))
+      for i, l in enumerate(item):
+        loss_array[i] = l
+        x_array[i] = xaxis[index][i]
+      min = np.amin(loss_array)
+      graphs.append(ROOT.TGraph(len(loss_array), x_array, loss_array))
+      graph = graphs[index] 
+      graph.SetLineColor(color)
+      graph.SetLineStyle(style)
+      mg.Add(graph)
+      mg.Draw('AL')
+      c.Update()
+      legend.AddEntry(graph, loss_ids[index] + " (min {:.4f})".format(min),"l")
+      color+=2
+      style+=1
+    mg.Draw('AL')
+    legend.Draw()
     c.Update()
     c.Print(filename)
 
-def PlotAccuracyRoot(loss_list, label, filename, color=2):
+def PlotAccuracyRoot(loss_list, loss_ids, xaxis, label, xlabel, filename, color=2, style=1):
     c = ROOT.TCanvas('c' ,"" ,200 ,10 ,700 ,500)
     c.SetGrid()
-    acc_array = np.zeros(len(loss_list))
-    x_array = np.zeros(len(loss_list))
-    for i, l in enumerate(loss_list):
-      acc_array[i] = 1-l
-      x_array[i] = i
-    graph = ROOT.TGraph(len(loss_list), x_array, acc_array)
-    graph.SetTitle(label + ";Epochs; Accuracy;")
-    graph.SetLineColor(color)
-    graph.GetXaxis
-    graph.Draw()
+    legend = ROOT.TLegend(.7, .1, .9, .3)
+    mg = ROOT.TMultiGraph()
+    mg.SetTitle("{};{}; accuracy(1-loss);".format(label, xlabel))
+    xaxis = xaxis[:len(loss_list[0])]
+    graphs=[]
+    for index, item in enumerate(loss_list):
+      acc_array = np.zeros(len(item))
+      x_array = np.zeros(len(item))
+      for i, l in enumerate(item):
+        acc_array[i] = 1-l
+        x_array[i] = xaxis[index][i]
+      max = np.amax(acc_array)
+      graphs.append( ROOT.TGraph(len(acc_array), x_array, acc_array))
+      graph = graphs[index]
+      graph.SetLineColor(color)
+      graph.SetLineStyle(style)
+      mg.Add(graph)
+      legend.AddEntry(graph, loss_ids[index] + " (max {:.4f})".format(max),"l")
+      color+=2
+      style+=1
+    mg.Draw('AL')
+    legend.Draw()
     c.Update()
     c.Print(filename)
+
+def equalize(list_of_lists):
+    for i, l in enumerate(list_of_lists):
+      if i ==0:
+        length = len(l)
+      else:
+        if len(l) < length:
+         length = len(l)
+         l = l[:length]
+    return list_of_lists
+
 
 if __name__ == "__main__":
    main()   
