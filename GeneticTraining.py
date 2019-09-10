db3e4e4e4f825e4b87dc08ab59d171ad9134f7e1
gulrukhk <gulrukhk@yahoo.com>
2019-09-10 03:49:30 -0700

    saving loss history per ietration

--- GeneticTraining.py
+++ GeneticTraining.py
@@ -1,103 +1,96 @@
 from __future__ import print_function
 from __future__ import division
-from loaders.HDF5torch import HDF5generator # generator for data
-seed = 0 # seed for reproducibility
+
+# setting seed
+seed = 0 
 import numpy as np
 np.random.seed(seed)
-import glob
-import time
 import torch
-#from memory_profiler import profile
-from analysis.utils import common as com
-# added for reproducibility
+
+# Also added for reproducibility
 torch.manual_seed(seed)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
+# imports from torch
 import torch.nn.functional as F
 from torchsummary import summary
-from architectures.GeneticArch import arch 
-from training.GA import GA
-import copy
 
+# user classes
+from loaders.HDF5torch import HDF5generator # generator for data
+from architectures.GeneticArch import arch  # architecture 
+from training.GA import GA                  # genetic training
+from analysis.utils import common as com    # utility functions
+
+# imports from python
+import copy
+import glob
+import time
+import argparse
+#from memory_profiler import profile 
 try:
     import cPickle as pickle
 except ImportError:
     import pickle
 
+# setting gpu
 try:
     import setGPU
 except:
     pass
 
-import argparse
-
+# command line arguments
 def get_parser():
   parser = argparse.ArgumentParser(description='GA Params' )
-  parser.add_argument('--epochs', action='store', type=int, default=1000, help='Number of epochs to train for.')
+  parser.add_argument('--epochs', action='store', type=int, default=50, help='Number of epochs to train for.')
   parser.add_argument('--batch_size', action='store', type=int, default=128, help='batch size per update')
   parser.add_argument('--train_ratio', action='store', default=0.9, help='test train ratio')
   parser.add_argument('--shuffle', action='store', default=False, help='shuffle data')
   parser.add_argument('--datapath', action='store', type=str, default="/bigdata/shared/LCD/NewV1/", help='base path for training data')
   parser.add_argument('--modelpath', action='store', type=str, default="results/models/", help='path to save model')
   parser.add_argument('--historypath', action='store', type=str, default="results/history/", help='path to save loss history')
-  parser.add_argument('--mutation_power', action='store', default=0.02, help='this parameter is similar to lr and controls change in value of weight to be mutated')
-  parser.add_argument('--mutation_rate', action='store', default=1, help='this parameter determines the number of weights to be mutated')
-  parser.add_argument('--num_parents', action='store', type=int, default=4, help='initial random population')
-  parser.add_argument('--num_children', action='store', type=int, default=4, help='number of children')
-  parser.add_argument('--num_events', action='store', type=int, default=5000, help='number of training events')
+  parser.add_argument('--mutation_power', action='store', default=1.0, help='this parameter controls the magnitude of weight updates/random weights')
+  parser.add_argument('--random_mutation', action='store', default=True, help='mutate weights randomly instead of adding updates')
+  parser.add_argument('--use_bias', action='store', default=False, help='using bias')
+  parser.add_argument('--mutation_rate', action='store', default=0.1, help='this parameter determines the number of weights to be mutated')
+  parser.add_argument('--num_parents', action='store', type=int, default=8, help='initial random population')
+  parser.add_argument('--num_children', action='store', type=int, default=8, help='number of children')
+  parser.add_argument('--num_events_train', action='store', type=int, default=5000, help='number of training events')
+  parser.add_argument('--num_events_test', action='store', type=int, default=10000, help='number of training events')
   parser.add_argument('--particles', nargs='+', default=['Ele'], help='particles used in training (Types should be written without quotes delimited by spaces)')
   parser.add_argument('--dim', action='store', type=int, default=2, help='Number of data dimensions to use')
   parser.add_argument('--dformat', action='store', type=str, default='channels_first')
-  parser.add_argument('--name', action='store', type=str, default="GA_5000events", help='identifier for a training')
+  parser.add_argument('--name', action='store', type=str, default="GA_training", help='identifier for a training')
   return parser
 
 # percentage loss
 def mape(pred, target):
    loss = torch.mean(torch.abs(target-pred)/target)
-   return loss
+   return 100.0 * loss
 
-def get_mutation_rate_plan5(mu, epoch, size):
+# get mutation rate according to epochs
+def get_mutation_rate(mu, epoch, total_epochs):
    if epoch < 10:
-     return 1.0
-   elif epoch < 100:
      return 0.1
-   else:
+   elif epoch < 20:
      return 0.01
-
-def get_mutation_rate_plan4(mu, epoch, size):
-   if epoch < 10:
-     return 1.0
-   elif epoch < 500:
-     return 0.1
    else:
-     return 0.01
+     return 0.001
 
-def get_mutation_rate_plan3(mu, epoch, size): #plan 3
-   if mu > 0.1:
-     return mu-0.01
-   else:
-     return 0.1
-
-#@profile
-def get_mutation_rate_plan1(mu, epoch, size): #plan 1
-   return 1.0
-
-#@profile
-def get_mutation_rate_plan2(mu, epoch, size): #plan 2
-   return 0.1
-
+# get mutation rate varying linearly from 0.1 to 0.001
 def get_mutation_linear(mp, epoch, total_epochs):
    return 0.1 - (((0.1-0.001) * epoch)/total_epochs)
 
+# get mutation rate decaying exponentially from 0.1 to 0.0001
 def get_mutation_exp(mp, epoch, total_epochs):
    #return 0.1 * np.exp(-6.0 * epoch/total_epochs)
    return 0.1 * np.exp(-6.9 * epoch/total_epochs)
 
+# get mutation rate with a decay factor
 def get_mutation_decay(mp, epoch, total_epochs):
    return 0.1 * 0.99**epoch
 
-def training():
+def GA_training():
    parser = get_parser()
    params = parser.parse_args()
    epochs = params.epochs
@@ -111,47 +104,66 @@ def training():
    historyfile = params.name + '_loss_history.pkl'
    particles = params.particles
    mutation_power = params.mutation_power
+   random_mutation = params.random_mutation
+   use_bias = params.use_bias
    mutation_rate = params.mutation_rate
    num_parents = params.num_parents
    num_children = params.num_children
-   num_evenst = params.num_events
+   num_events_train = params.num_events_train
+   num_events_test = params.num_events_test
    dim = params.dim
    dformat = params.dformat
    input_shape = com.get_input_shape(dformat, dim)
        
    # define GA training object
    ga = GA(arch=arch, mutation_rate = mutation_rate, mutation_power = mutation_power)
+   
+   # setting up training params
    ga.num_parents = num_parents
    ga.num_children = num_children
-   ga.random_update = True
+   ga.random_update = random_mutation
+   ga.use_bias = use_bias
+
+   # print info about architecture
    summary(ga.arch().cuda(), input_size=input_shape)
    print('The number of paramerets is {}.'.format(ga.size))
+
+   # divide available data files into train and test   
+   train_files, test_files = com.divide_train_test(datapath, particles, train_ratio)
+
+   # initialization
+   train_loss_list = []
+   train_loss_list_batch = []
+   test_loss_list = []
+   time_list =[]
+   time_list_batch =[]
+   best_loss = 100
+   batch_iter = 0
+
    # create random population
    parents = ga.random_population()
    criterion = mape
-   
-   train_files, test_files = com.divide_train_test(datapath, particles, train_ratio)
-   loss_list=[]
-   train_loss_list = []
-   test_loss_list = []
-   time_list =[]
-   best_loss = 100
-   batch_iter = 0
+
+   # start training
    for epoch in range(epochs):
      init = time.time()
-     # Test the model
-     train_files = train_files[:1]     
-     train_loader = HDF5generator(train_files, batch_size=batch_size, shuffle=shuffle, num_events=num_events)
+     
+     # initialize data loader
+     train_loader = HDF5generator(train_files, batch_size=batch_size, shuffle=shuffle, num_events=num_events_train)
      if epoch ==0:
-        total_iter = train_loader.total_batches * epochs
-        print(total_iter) 
-     loss =[]
-     print('Train generator initialized in {} sec.'.format(time.time()-init))
-     ga.mutation_rate = get_mutation_rate_plan2(ga.mutation_rate, epoch, ga.size)
+        total_iter = train_loader.total_batches * epochs # total training iterations or batches
+     
+     # get the mutation rate for current epoch/batch 
+     ga.mutation_rate = get_mutation_rate(ga.mutation_rate, epoch, epochs)
+     
+     # training for current epoch
      for i, data in enumerate(train_loader):
+        init_batch = time.time()
+
+        # For first batch of first epoch evaluate parent population
         if (epoch==0) and (i==0):
           print('Evaluating random population.........')
-          for i, model in enumerate(parents):
+          for j, model in enumerate(parents):
             model.eval()
             with torch.no_grad():
               outputs = model(data['ECAL'])
@@ -160,10 +172,17 @@ def training():
                 best_loss = loss.item()
                 best_parent = copy.deepcopy(model)
                 print('best parent', best_loss)
+          
+          # keep only best parent and delete the rest
           parents = []
-        ga.mutation_power = get_mutation_exp(ga.mutation_power, batch_iter, total_iter)
-        batch_iter+=1
+       
+        # get mp if not using random mutation
+        if not random_mutation:
+           ga.mutation_power = get_mutation_exp(ga.mutation_power, batch_iter, total_iter)
+        # mutate best parent
         children = ga.return_children(best_parent)
+        
+        # evaluate children
         for j, model in enumerate(children):
            model.eval()
            with torch.no_grad():
@@ -173,14 +192,20 @@ def training():
                 best_loss = loss.item()
                 best_parent = copy.deepcopy(model)
                 print('best child loss = {}'.format(best_loss))
+        
+        # update train loss history for batch
+        train_loss_list_batch.append(best_loss)
+        time_list_batch.append(time.time()-init_batch) 
+        batch_iter+=1
      
+     # update epoch loss history
      train_loss_list.append(best_loss)
      time_list.append(time.time()-init)
-     print('Epoch={} Mutation rate = {} Mutation power = {} Time taken ={} '.format(epoch, ga.mutation_rate, ga.mutation_power, time.time()-init))    
+     
      # Test the model
-     test_loader = HDF5generator(test_files, batch_size=batch_size, shuffle=shuffle, num_events=20000)
-     best_parent.eval()
      test_init = time.time()
+     test_loader = HDF5generator(test_files, batch_size=batch_size, shuffle=shuffle, num_events=num_events_test)
+     best_parent.eval()
      with torch.no_grad():
        total = 0
        loss_list=[]
@@ -188,13 +213,21 @@ def training():
          outputs = best_parent(data['ECAL'])
          loss = criterion(outputs, data['target'])
          loss_list.append(loss.item())
-       test_loss_list.append(np.mean(loss_list))
+       
+     # update test loss history
+     test_loss_list.append(np.mean(loss_list))
+
+     # display info
+     print('Epoch = {}'.format(epoch))
+     print('Train loss = {:.4f}  %   Train time ={:.4f} sec'.format(train_loss_list[-1], time_list[-1]))
+     print('Test loss  = {:.4f}  %   Test time = {:.4f} sec'.format(test_loss_list[-1], time.time()-test_init))
      print('================================================================')
-     print('Train loss = {} Test loss={}'.format(train_loss_list[-1], test_loss_list[-1]))
-     print('Test time = {} sec'.format(time.time()-test_init))
-     pickle.dump({'train': train_loss_list, 'test': test_loss_list, 'timing':time_list}, open(HISTORY_PATH + historyfile, 'wb'))
-     # Save the model and plot
+
+     # save loss history
+     pickle.dump({'train': train_loss_list, 'test': test_loss_list, 'timing':time_list, 'train_batch': train_loss_list_batch, 'timing_batch':time_list_batch}, open(HISTORY_PATH + historyfile, 'wb'))
+     
+     # save the model
      torch.save(model.state_dict(), MODEL_STORE_PATH + modelfile)
 
 if __name__ == "__main__":
-    training()
+    GA_training()
--- training/GA.py
+++ training/GA.py
@@ -6,23 +6,28 @@ import numpy as np
 
 class GA():
    def __init__(self, arch, mutation_rate=0.01, mutation_power=0.02):
-     #super(GA, self).__init__()
-     self.mutation_rate= mutation_rate 
+     # initial params     
+     self.mutation_rate= mutation_rate  
      self.mutation_power = mutation_power
      self.weight_init= torch.nn.init.xavier_uniform_
      self.arch = arch
      self.size = self.count_parameters(arch())
      self.num_parents = 1
      self.num_children = 4
+     self.random_update = False
+     self.use_bias = True
           
+   # count net params
    def count_parameters(self, model):
      return sum(p.numel() for p in model.parameters())
 
+   # initialize weights
    def init_weights(self, m):
      if ((type(m) == nn.Linear) | (type(m) == nn.Conv2d)):
       self.weight_init(m.weight)
       m.bias.data.fill_(0.00)
    
+   # make a set of random individuals 
    def random_population(self):
      parents = []
      for _ in range(self.num_parents):
@@ -34,21 +39,46 @@ class GA():
         parents.append(parent)
      return parents
 
+   # mutate an individual
    def mutate(self, parent):
+     # new individual is a copy of old
      child = copy.deepcopy(parent)
+     
+     # calculate number of mutations to apply and get random indexes
      param_size = self.count_parameters(parent)
      mutation_size = int(param_size * self.mutation_rate)
-     genes_to_mutate = np.random.randint(param_size, size=mutation_size)
-     #genes_to_mutate = sorted(genes_to_mutate)
-     index = 0
-     for p in child.parameters():
-       for gene in genes_to_mutate[index:]:
-         if gene < np.prod(p.size()):
-           loc = np.unravel_index(gene, p.size())
-           p[loc]+= self.mutation_power * np.random.randn()
-           index +=1
+     genes_to_mutate = np.random.randint(param_size, size=mutation_size) # random indexes
+
+     # sort indexes in ascending prder
+     genes_to_mutate.sort() # sort in ascending order
+
+     # get an array of updates
+     update = self.mutation_power * np.random.randn(mutation_size) # random updates
+     
+     # update weights
+     index = 0 # genes updated
+     prev_size = 0 # size of all previous layers
+     for num, p in enumerate(child.parameters()): # loop through layers
+       for i, gene in enumerate(genes_to_mutate[index:]): # loop through indexes to be mutated
+         if gene < (prev_size + np.prod(p.size())): # if gene in current layer
+           loc = np.unravel_index(gene - prev_size, p.size()) # location of weight corressponding to gene
+           if self.random_update:
+              new_val = update[index] # updated weight will be equal to random mutation
+           else:
+              new_val = p[loc] + update[index] # updated weight will be equal to sum of weight and random mutation
+
+           # update weights
+           if self.use_bias:
+             p[loc]= new_val
+           elif len(p.shape)> 1: # to remove bias
+             p[loc]= new_val
+           index +=1 
+       
+       # update previous param size
+       prev_size+= np.prod(p.size())
      return child
  
+   # get mutated individuals
    def return_children(self, parent):
      children = []
      for i in range(self.num_children):
